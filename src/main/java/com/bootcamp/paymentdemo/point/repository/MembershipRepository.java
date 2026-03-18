ade903953ac46b1ece6e9c20603c600ef94ce802
rioloe <rioloelux@gmail.com>
2026-03-18 16:53:46 +0900

    Refactor: Membership.java 삭제

--- src/main/java/com/bootcamp/paymentdemo/point/repository/MembershipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.paymentdemo.point.repository;
-
-import com.bootcamp.paymentdemo.point.entity.Membership;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface MembershipRepository extends JpaRepository<Membership, Long> {
-    Optional<Membership> findByUserId(Long userId);
-}
--- src/main/java/com/bootcamp/paymentdemo/point/service/PointService.java
+++ src/main/java/com/bootcamp/paymentdemo/point/service/PointService.java
@@ -1,7 +1,6 @@
 package com.bootcamp.paymentdemo.point.service;
 
 import com.bootcamp.paymentdemo.point.repository.MembershipPolicyRepository;
-import com.bootcamp.paymentdemo.point.repository.MembershipRepository;
 import com.bootcamp.paymentdemo.point.repository.PointTransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +13,6 @@ public class PointService {
 
     private final PointTransactionRepository pointTransactionRepository;
     private final MembershipPolicyRepository membershipPolicyRepository;
-    private final MembershipRepository membershipRepository;
 
     // 포인트 적립
     @Transactional
