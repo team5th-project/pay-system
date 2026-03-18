5169fd6f23398104e5fc384de870911df59301a0
kwonjiwon <kjw81024@gmail.com>
2026-03-18 12:19:35 +0900

    Fix: unified urls by using RequestMapping annotation

--- src/main/java/com/bootcamp/paymentdemo/product/ProductController.java
+++ src/main/java/com/bootcamp/paymentdemo/product/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,11 +17,12 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
 
     //1. 상품 다건 조회
-    @GetMapping("/api/products")
+    @GetMapping
     ResponseEntity<CommonResponse<List<GetProductResponse>>> getProducts(){
         List<GetProductResponse> responses = productService.getProducts();
         return CommonResponseHandler.success(HttpStatus.OK, responses);
@@ -28,7 +30,7 @@ public class ProductController {
     }
 
     //2. 상품 단건 조회
-    @GetMapping("/api/products/{productId}")
+    @GetMapping("/{productId}")
     ResponseEntity<CommonResponse<GetProductResponse>> getOne(@PathVariable Long productId){
         GetProductResponse response = productService.getOne(productId);
 //        return ResponseEntity.status(HttpStatus.OK).body(response);
