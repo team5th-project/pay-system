bcde2e757000b278de94a6f7df3a2524b0b3d47e
soyeong <thdud001012@gmail.com>
2026-03-18 16:45:40 +0900

    Fix:오타 수정

--- src/main/java/com/bootcamp/paymentdemo/payment/entity/Payment.java
+++ src/main/java/com/bootcamp/paymentdemo/payment/entity/Payment.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name="payments")
 public class Payment extends BaseEntity {
