2bb8491739fd1b7e2f0e24129512d072308bba73
soyeong <thdud001012@gmail.com>
2026-03-19 14:42:54 +0900

    Feat:RestClientConfig 초안 작성 완료

---
+++ src/main/java/com/bootcamp/paymentdemo/common/config/RestClientConfig.java
@@ -0,0 +1,27 @@
+package com.bootcamp.paymentdemo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean("portOneRestClient")
+    public RestClient portOneRestClient(PortOneProperties properties){
+
+            SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); // 5초 안에 연결 안 되면 포기
+            factory.setReadTimeout(30000);   // 연결 후 30초 안에 응답 안 오면 포기
+
+            return RestClient.builder()
+                    .requestFactory(factory)
+                    .baseUrl(properties.getApi().getBaseUrl())
+                    .defaultHeader("Content-Type", "application/json")
+                    .defaultHeader("Authorization","PortOne "+ properties.getApi().getSecret())
+                    .build();
+    }
+
+
+}
