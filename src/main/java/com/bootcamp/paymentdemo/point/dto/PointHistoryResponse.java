5bc5824128dbba1e0861de3779e300db44e27961
rioloe <rioloelux@gmail.com>
2026-03-19 20:55:45 +0900

    Feat: 포인트 방어 코드 추가 및 에러코드 추가

--- src/main/java/com/bootcamp/paymentdemo/point/dto/PointHistoryResponse.java
+++ src/main/java/com/bootcamp/paymentdemo/point/dto/PointHistoryResponse.java
@@ -6,6 +6,8 @@ import java.util.List;
 public record PointHistoryResponse(
         List<PointTransactionItem> pointHistories
 ) {
+    // TODO: 현재 전체 조회 방식으로 데이터가 많아지면 서버 부하 발생 가능
+    //       추후 팀 협의 후 페이징 처리 예정
     public static PointHistoryResponse from(List<PointTransaction> transactions) {
         return new PointHistoryResponse(
                 transactions.stream()
