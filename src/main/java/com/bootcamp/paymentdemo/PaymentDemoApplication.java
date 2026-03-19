eb017d970fce763cb92a35228feac665a7b1eb9c
rioloe <rioloelux@gmail.com>
2026-03-20 03:35:17 +0900

    feat: 포인트 환불 및 소멸 구현

--- src/main/java/com/bootcamp/paymentdemo/PaymentDemoApplication.java
+++ src/main/java/com/bootcamp/paymentdemo/PaymentDemoApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class PaymentDemoApplication {
