d5273f6b8b746d337ad22fcbbf3cb25b7d544c51
kwonjiwon <kjw81024@gmail.com>
2026-03-20 11:56:37 +0900

    Feat: deductPoint, addPoint part fixed, total point part featured

--- src/main/java/com/bootcamp/paymentdemo/user/entity/User.java
+++ src/main/java/com/bootcamp/paymentdemo/user/entity/User.java
@@ -62,20 +62,50 @@ public class User extends BaseEntity {
         this.membershipGrade = MembershipGrade.NORMAL;
     }
 
-    // User 엔티티에 추가
+    // 포인트 차감
     public void deductPoint(int points) {
+        if (points <= 0) {
+            throw new ServiceException(ErrorCode.INVALID_POINT_AMOUNT);
+        }
         if (this.pointBalance < points) {
             throw new ServiceException(ErrorCode.INSUFFICIENT_POINT);
         }
         this.pointBalance -= points;
     }
-
+    // 포인트 적립
+// 주문 확정시 멤버십 등급에 따라 포인트 적립
     public void addPoint(int points) {
+        if (points <= 0) {
+            throw new ServiceException(ErrorCode.INVALID_POINT_AMOUNT);
+        }
         this.pointBalance += points;
     }
-
+    // 멤버십 등급 변경
+// 결제 완료 및 환불 시 totalOrderAmount 기준으로 등급 재계산 후 호출
     public void updateGrade(MembershipGrade grade) {
         this.membershipGrade = grade;
+    }
+
+    // 누적 주문금액 증가
+// 결제 완료 시 호출, 멤버십 등급 산정 기준값 사용
+    public void addTotalOrderAmount(long paymentAmount) {
+        if (paymentAmount <= 0) {
+            throw new ServiceException(ErrorCode.INVALID_PAYMENT_AMOUNT);
+        }
+        this.totalOrderAmount += paymentAmount;
+    }
+
+    // 누적 주문금액 차감
+// 환불 완료 시 호출, 차감 후 0 미만 되는 경우 0으로 초기화
+    public void deductTotalOrderAmount(long refundAmount) {
+        if (refundAmount <= 0) {
+            throw new ServiceException(ErrorCode.INVALID_PAYMENT_AMOUNT);
+        }
+        if (this.totalOrderAmount < refundAmount) {
+            this.totalOrderAmount = 0L;
+            return;
+        }
+        this.totalOrderAmount -= refundAmount;
     }
 
 
