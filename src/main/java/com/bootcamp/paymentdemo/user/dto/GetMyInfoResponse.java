a6ec1b896f68fd43f9139391096aed4b70618acd
kwonjiwon <kjw81024@gmail.com>
2026-03-24 14:52:58 +0900

    Fix: user domain getMyInfo dto fixed

--- src/main/java/com/bootcamp/paymentdemo/user/dto/GetMyInfoResponse.java
+++ src/main/java/com/bootcamp/paymentdemo/user/dto/GetMyInfoResponse.java
@@ -3,17 +3,23 @@ package com.bootcamp.paymentdemo.user.dto;
 import com.bootcamp.paymentdemo.user.entity.User;
 
 public record GetMyInfoResponse(
-        Long id,
+        String id,
         String email,
         String name,
-        String phone
+        String phone,
+        String totalAmount,
+        String rank,
+        String availablePoint
 ) {
-    public static GetMyInfoResponse of(User user){
+    public static GetMyInfoResponse of(User user, int availablePoint){
         return new GetMyInfoResponse(
-                user.getId(),
+                user.getId().toString(),
                 user.getEmail(),
                 user.getName(),
-                user.getPhone()
+                user.getPhone(),
+                user.getTotalOrderAmount().toString(),
+                user.getMembershipGrade().toString(),
+                String.valueOf(availablePoint)
         );
     }
 }
